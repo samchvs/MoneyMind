90b3b957a8aa2d0bfda93b14e38bbeaf14cdb749
samchvs <chavessamantha019@gmail.com>
2025-04-13 02:55:16 +0800

    action fix

--- Actions.js
+++ Actions.js
@@ -1,17 +1,19 @@
 import { Animated } from 'react-native';
 
-export const moveLogoUp = (animatedValue, distance) => {
+
+export const moveLogoUp = (animatedValue, toValue) => {
   Animated.timing(animatedValue, {
-    toValue: distance,  
-    duration: 1000,
+    toValue,         
+    duration: 2000,    // Increased duration for smooth movement
     useNativeDriver: true,
   }).start();
 };
 
+// Fade in the MoneyMind logo
 export const fadeInMoneyMind = (moneyMindOpacity) => {
   Animated.timing(moneyMindOpacity, {
-    toValue: 1, 
-    duration: 2000, 
+    toValue: 1,      // Fade in to full opacity
+    duration: 1500,  // Adjusted duration for smoother fade
     useNativeDriver: true,
   }).start();
 };
