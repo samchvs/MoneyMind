18603bc58700fef859cc2fca07837c0ef43bd259
samchvs <chavessamantha019@gmail.com>
2025-05-08 00:31:18 +0800

    done

--- AiPage.js
+++ AiPage.js
@@ -1,35 +1,39 @@
 import React from 'react';
 import { View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient'; 
+import { useNavigation } from '@react-navigation/native';
+
 
 const AiPage = () => {
-  return (
-    <LinearGradient
-      colors={['#000000', '#171717', '#232323', '#3b3b3b', '#4f4f4f']}
-      style={styles.container}
-    >
-      <Text style={styles.title}>Generate with AI!</Text>
-      <Text style={styles.subtitle}>AI Budget Recommendation</Text>
+    const navigation = useNavigation();
+    return (
+        <LinearGradient
+        colors={['#000000', '#171717', '#232323', '#3b3b3b', '#4f4f4f']}
+        style={styles.container}
+        >
+        <Text style={styles.title}>Generate with AI!</Text>
+        <Text style={styles.subtitle}>AI Budget Recommendation</Text>
 
-    <Image
-        source={require('./assets/logo.png')} 
-        style={styles.logo}
-      />
-    
-    <View style={styles.buttonContainer}>
-  <TouchableOpacity style={styles.button} onPress={() => console.log('Button Pressed')}>
-    <View style={styles.buttonContent}>
-      <Image
-        source={require('./assets/spark.png')}
-        style={{ width: 20, height: 20, marginRight: 10 }}
-      />
-      <Text style={styles.buttonText}>Generate</Text>
-    </View>
-  </TouchableOpacity>
-</View>
+        <Image
+            source={require('./assets/logo.png')} 
+            style={styles.logo}
+        />
+        
+        <View style={styles.buttonContainer}>
+        <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('GenAiPage')}
+        >
+            <View style={styles.buttonContent}>
+            <Image
+                source={require('./assets/spark.png')}
+                style={{ width: 20, height: 20, marginRight: 10 }}
+            />
+            <Text style={styles.buttonText}>Generate</Text>
+            </View>
+        </TouchableOpacity>
+        </View>
 
-    </LinearGradient>
-  );
+        </LinearGradient>
+    );
 };
 
 const styles = StyleSheet.create({
